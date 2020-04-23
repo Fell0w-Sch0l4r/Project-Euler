867aebabf4b1e403905f12114a1397ab7fe58a35
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-04-23 22:27:08 +0000

    fiz os exercícios 1,2,4,5,6,7

--- 1-50.py
+++ 1-50.py
@@ -26,8 +26,8 @@ for t in range (100,1000):
             a=t*r
 print(a)'''
 
-# 5    232792560
-a=0
+# 5
+'''a=0
 b=0
 while b!=20:
     a+=1
@@ -35,4 +35,31 @@ while b!=20:
     for t in range (1,21):
         if a%t==0:
             b+=1
-print(a)
+        else:
+            break
+print(a)'''
+
+#6
+'''a=0
+for t in range (1,101):
+    a+=t**2
+b=0
+for r in range (1,101):
+    b+=r
+b**=2
+print(b-a)'''
+
+#7
+'''a=0
+b=0
+while a!=10001:
+    d = 0
+    b += 1
+    for c in range (b,0,-1):
+        if b%c==0:
+            d+=1
+    if d==2:
+        a+=1
+print(b)'''
+
+#8
