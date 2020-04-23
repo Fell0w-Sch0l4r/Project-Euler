9b25e4263ccca5f2f8478b8d407a80ebe9d6ee93
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-04-23 21:32:06 +0000

    versão 1.0 o exercício 5

--- 1-50.py
+++ 1-50.py
@@ -6,3 +6,33 @@ for t in range (1,1000):
 print(a)'''
 
 #2
+#3
+
+'''a=int(input("Digit the number to calculate the prime factors:"))
+b=2
+while a!=1:
+    if a%b==0:
+        print(f"{a:<3}|",end=" ")
+        a//=b
+        print(b)
+    b+=1
+print(a)'''
+
+# 4
+'''a=0
+for t in range (100,1000):
+    for r in range (100,1000):
+        if str(t*r)==str(t*r)[::-1] and t*r>a:
+            a=t*r
+print(a)'''
+
+# 5    232792560
+a=0
+b=0
+while b!=20:
+    a+=1
+    b=0
+    for t in range (1,21):
+        if a%t==0:
+            b+=1
+print(a)
